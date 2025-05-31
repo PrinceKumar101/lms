2f808a695799d5d6db97ce06f17576b14dbecee5
Prince Kumar <149325883+PrinceKumar101@users.noreply.github.com>
2025-05-31 18:02:35 +0530

    config index.ts

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+
+
+const port = process.env.Port || 3000;
+app.listen(port, () => {
+  console.log("Server running at port " + port);
+});
