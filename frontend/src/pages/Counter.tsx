1e2dcb086391978bc0aaecfc4c1f17a5caa87ad6
Prince Kumar <princekumar201926@gmail.com>
2025-06-21 00:21:52 +0530

    forntend setup

---
+++ frontend/src/pages/Counter.tsx
@@ -0,0 +1,45 @@
+import { useSetRecoilState, useRecoilValue, useRecoilState } from "recoil";
+import { counterAtom } from "../store/atoms/counter";
+
+const Increase = () => {
+  const setCount = useSetRecoilState(counterAtom);
+  return (
+    <button
+      onClick={() => {
+        setCount((currentValue) => currentValue + 1);
+      }}
+    >
+      Increase
+    </button>
+  );
+};
+
+const Decrease = () => {
+  const setCount = useSetRecoilState(counterAtom);
+  return (
+    <button
+      onClick={() => {
+        setCount((currentValue) => currentValue - 1);
+      }}
+    >
+      Decrease
+    </button>
+  );
+};
+
+const DisplayCount = () => {
+  const count = useRecoilValue(counterAtom);
+  return <div>{count}</div>;
+};
+
+const Counter = () => {
+  return (
+      <div className="text-4xl flex flex-col gap-5">
+        <Increase />
+        <Decrease />
+        <DisplayCount />
+      </div>
+  );
+};
+
+export default Counter;
