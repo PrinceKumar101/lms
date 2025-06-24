65fee51a5ce431337447769b668dd50fcdabc8e4
Prince Kumar <princekumar201926@gmail.com>
2025-06-24 22:58:08 +0530

    bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import Page from './pages/Page'
+import { RecoilRoot } from "recoil";
+
+import AtomFamilyUsage from "./pages/AtomFamily";
 
 const App = () => {
   return (
     <>
-    <Page/>
+      <RecoilRoot>
+        <div className="bg-black text-white h-screen flex flex-col gap-2 justify-center items-center">
+          <AtomFamilyUsage />
+        </div>
+      </RecoilRoot>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
