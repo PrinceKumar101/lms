e6c0228a0afa569cd6d117363aae0914a199378e
Prince Kumar <princekumar201926@gmail.com>
2025-06-28 23:54:27 +0530

    bug fix frontend

---
+++ frontend/src/pages/CustomUi.tsx
@@ -0,0 +1,10 @@
+import Button from "../components/UI/Button";
+
+const CustomUI = () => {
+  return (
+    <>
+      <Button innerText="Click Me" varient="primaryVarient" size="medium" />
+    </>
+  );
+};
+export default CustomUI;
