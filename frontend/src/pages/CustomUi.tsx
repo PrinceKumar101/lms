9949fd3af568ba18023ea854a96aeb1d044b7d42
Prince Kumar <princekumar201926@gmail.com>
2025-06-29 10:00:00 +0530

    frontend fix

--- frontend/src/pages/CustomUi.tsx
+++ frontend/src/pages/CustomUi.tsx
@@ -1,9 +1,9 @@
-import Button from "../components/UI/Button";
+import { Button } from "@/components/UI/Button";
 
 const CustomUI = () => {
   return (
     <>
-      <Button innerText="Click Me" varient="primaryVarient" size="medium" />
+    <Button children={"Hii"}  variant={"ghost"}  className=" text-2xl font-bold text-sky-500 bg-white hover:text-9xl hover:p-10 hover:text-green-500 ease-in-out duration-800 "/>
     </>
   );
 };
