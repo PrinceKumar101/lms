534341fdd7dac31ad1c11e3817c48b12618da0f2
Prince Kumar <princekumar201926@gmail.com>
2025-06-29 10:00:00 +0530

    "third commit[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;3D[D[D[D[

--- frontend/src/pages/CustomUi.tsx
+++ frontend/src/pages/CustomUi.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/UI/Button";
 const CustomUI = () => {
   return (
     <>
-    <Button children={"Hii"}  variant={"ghost"}   className=" text-2xl font-bold text-sky-500 bg-white hover:text-9xl hover:p-10 hover:text-green-500 ease-in-out duration-800 "/>
+    <Button children={"Hii"}  variant={"ghost"}   className="  text-2xl font-bold text-sky-500 bg-white hover:text-9xl hover:p-10 hover:text-green-500 ease-in-out duration-800 "/>
     </>
   );
 };
