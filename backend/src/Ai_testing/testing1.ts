023116790aefd8360c5b712f9b83466ab61a7303
Prince Kumar <princekumar201926@gmail.com>
2025-09-02 01:27:49 +0530

    ai testing variable fix

--- backend/src/Ai_testing/testing1.ts
+++ backend/src/Ai_testing/testing1.ts
@@ -2,11 +2,11 @@ import Cloudflare from "cloudflare";
 
 export const AiTesting = async () => {
     const client = new Cloudflare({
-        apiToken: "QzoHjbIXF_omPdYSbYk3A5mw_xEAqrEVICea22CN", 
+        apiToken: process.env.Cloudflare_api_token, 
     });
 
     const response = await client.ai.run("@cf/black-forest-labs/flux-1-schnell", {
-        account_id: "750250527aa24e3c4d35a9a1a523ae86",
+        account_id: process.env.Cloudflare_account_id ?? "",
         prompt: "make a house with cool morning backdrop",
     });
     return response;
